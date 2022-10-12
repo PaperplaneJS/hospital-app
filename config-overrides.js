ab2d83043e2ee3a68fb0ecec011f86d8fe32edeb
jia-niang <1@paperplane.cc>
2022-10-12 14:28:10 +0800

    feat: init

---
+++ config-overrides.js
@@ -0,0 +1,28 @@
+const {
+  override,
+  fixBabelImports,
+  addWebpackAlias,
+  addLessLoader,
+  addBabelPlugins
+} = require('customize-cra')
+
+module.exports = {
+  webpack: override(
+    addLessLoader({
+      lessOptions: {
+        noIeCompat: true,
+        javascriptEnabled: true
+      }
+    }),
+
+    addWebpackAlias({ '@': 'src/' }),
+
+    addBabelPlugins(['@emotion']),
+
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true
+    })
+  )
+}
