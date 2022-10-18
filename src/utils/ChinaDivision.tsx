68a2e1f1d1f213992c0f691987c0bc8adee183db
jia-niang <1@paperplane.cc>
2022-10-18 18:09:14 +0800

    refactor: 删除大小写更改的文件

--- src/utils/ChinaDivision.tsx
+++
@@ -1,8 +0,0 @@
-import provincesAndCities from 'china-division/dist/pc-code.json'
-import { omit } from 'lodash'
-
-export type IPosition = TypeofArray<typeof provincesAndCities>
-
-export const provinces = provincesAndCities.map(province => omit(province, 'children'))
-
-export { provincesAndCities }
