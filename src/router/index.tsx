457a66c6f299e0396fb620ca832c04d8bbf1a203
jia-niang <1@paperplane.cc>
2023-02-10 12:35:18 +0800

    chore+feat: 使用 /hospital 子目录

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,10 +3,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Admin from '@/pages/Admin'
 import Home from '@/pages/Home'
 
-const router = createBrowserRouter([
-  { path: '/admin', element: <Admin /> },
-  { path: '/', element: <Home /> },
-])
+const router = createBrowserRouter(
+  [
+    { path: '/admin', element: <Admin /> },
+    { path: '/', element: <Home /> },
+  ],
+  { basename: process.env.PUBLIC_URL }
+)
 
 function RouterEntry(): RC {
   return <RouterProvider router={router} />
--- src/utils/client.ts
+++ src/utils/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const client = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.PUBLIC_URL + '/api',
   timeout: 15000,
 })
 
