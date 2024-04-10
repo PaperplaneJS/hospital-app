b3c6b96be66ab176fbdf46fbd4a371f037be4d44
jia-niang <1@paperplane.cc>
2024-04-11 00:55:01 +0800

    chore(ci): 现在支持将网站资源上传 CDN

--- config-overrides.js
+++ config-overrides.js
@@ -5,7 +5,11 @@ const {
   addWebpackAlias,
   fixBabelImports,
   addWebpackModuleRule,
+  addWebpackPlugin,
 } = require('customize-cra')
+const S3Plugin = require('webpack-s3-plugin')
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin')
+const noop = require('lodash/identity')
 
 module.exports = {
   webpack: override(
@@ -39,7 +43,44 @@ module.exports = {
     addWebpackModuleRule({
       test: /\.scss$/,
       use: ['style-loader', 'css-loader', 'sass-loader'],
-    })
+    }),
+
+    addWebpackPlugin(
+      new HtmlWebpackTagsPlugin({
+        usePublicPath: false,
+        links: [
+          { path: 'https://cdn.paperplane.cc', attributes: { rel: 'dns-prefetch' } },
+          { path: 'https://cdn.paperplane.cc', attributes: { rel: 'preconnect' } },
+        ],
+      })
+    ),
+
+    process.env.NODE_ENV === 'production' && process.env.COS_SECRET_ID && process.env.COS_SECRET_KEY
+      ? addWebpackPlugin(
+          new S3Plugin({
+            exclude: /.*\.html$/,
+            basePath: 'paperplane-hospital',
+            s3Options: {
+              accessKeyId: process.env.COS_SECRET_ID,
+              secretAccessKey: process.env.COS_SECRET_KEY,
+              region: 'ap-hongkong',
+              endpoint: 'https://cos.ap-hongkong.myqcloud.com',
+              apiVersion: '2006-03-01',
+            },
+            s3UploadOptions: {
+              Bucket: 'paperplane-cdn-1253277322',
+            },
+          })
+        )
+      : noop,
+
+    process.env.NODE_ENV === 'production' && process.env.COS_SECRET_ID && process.env.COS_SECRET_KEY
+      ? function setPublicPath(config) {
+          config.output.publicPath = '//cdn.paperplane.cc/paperplane-hospital/'
+
+          return config
+        }
+      : noop
   ),
 
   devServer: overrideDevServer(devServerConfig => ({
